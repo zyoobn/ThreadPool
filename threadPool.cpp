4b0bb12015576d7c88de6e93766f7603137ec59f
zyoobn <100299392+zyoobn@users.noreply.github.com>
2024-01-26 00:28:48 +0800

    Add files via upload

--- threadPool.cpp
+++ threadPool.cpp
@@ -5,22 +5,30 @@ std::mutex printMtx;
 
 int main() {
     threadPool tp(5);
-    for (int i = 1; i <= 10; ++i) {
-        tp.enqueue([] (int i, int j) {
+    for (int i = 1; i <= 5; ++i) {
+        int a = tp.enqueue([] (int i, int j) {
             std::unique_lock<std::mutex> lock(printMtx);
+            std::cout << std::this_thread::get_id() << std::endl;
             std::cout << "1 : " << i << " + " << j << " = " << i + j << std::endl; 
             // std::this_thread::sleep_for(std::chrono::seconds(1));
-        }, i, i * 10);
-        tp.enqueue([] (int i) {
+            return i + j;
+        }, i, i * 10).get();
+        int b = tp.enqueue([a] (int i) {
             std::unique_lock<std::mutex> lock(printMtx);
+            std::cout << std::this_thread::get_id() << std::endl;
             std::cout << "2 : " << i << std::endl; 
             // std::this_thread::sleep_for(std::chrono::seconds(1));
-        }, i);
-        tp.enqueue([] () {
+            return a + i;
+        }, i).get();
+        tp.enqueue([a, b] () {
             std::unique_lock<std::mutex> lock(printMtx);
+            std::cout << std::this_thread::get_id() << std::endl;
             std::cout << "3 : " << "over" << std::endl; 
             // std::this_thread::sleep_for(std::chrono::seconds(1));
         });
+        std::unique_lock<std::mutex> lock(printMtx);
+        std::cout << std::this_thread::get_id() << std::endl;
+        std::cout << a << ' ' << b << std::endl;
     }
     return 0;
 }
